8afc5f4286489da8aa4f8ced659baa0dd80defef
tnc1997 <tnc1997@virginmedia.com>
2017-04-19 20:45:11 +0100

    feat(courses): show course location on details page

--- src/providers/buildings.ts
+++ src/providers/buildings.ts
@@ -1,4 +1,5 @@
 import {Injectable} from '@angular/core';
+import {Api} from './api';
 import {Building} from '../models/building';
 import 'rxjs/add/operator/map';
 
@@ -6,7 +7,7 @@ import 'rxjs/add/operator/map';
 export class BuildingProvider {
   buildings: Building[] = [];
 
-  constructor() {
+  constructor(public api: Api) {
     let buildings = [
       {code: "be", name: "Bedford Building", lat: 51.88788, lng: -2.08754},
       {code: "bl", name: "Broadlands Lodge", lat: 51.88574, lng: -2.09028},
@@ -54,5 +55,23 @@ export class BuildingProvider {
     }
 
     return building;
+  }
+
+  queryBuildings(id?: Number) {
+    return new Promise((resolve, reject) => {
+      this.api.getEntity("buildings", id).map(res => res.json()).subscribe((values) => {
+        let buildings = [];
+
+        if (values != null && values.length > 0) {
+          for (let i = 0; i < values.length; i++) {
+            buildings.push(new Building(values[i].building_code, values[i].building_name, values[i].building_lat, values[i].building_lng));
+          }
+        }
+
+        resolve(buildings);
+      }, (error) => {
+        reject(error);
+      });
+    });
   }
 }
