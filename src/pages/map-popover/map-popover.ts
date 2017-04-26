9d79b55506424b267812f2aede62b8db32804028
tnc1997 <tnc1997@virginmedia.com>
2017-04-26 20:39:32 +0100

    feat(map): toggle to display the building overlay

--- src/pages/map-popover/map-popover.ts
+++ src/pages/map-popover/map-popover.ts
@@ -1,7 +1,5 @@
 import {Component} from '@angular/core';
 import {NavController, NavParams} from 'ionic-angular';
-import {Building} from '../../models/building';
-import {MapDirections} from '../map-directions/map-directions';
 
 @Component({
   selector: 'page-map-popover',
@@ -9,12 +7,30 @@ import {MapDirections} from '../map-directions/map-directions';
 })
 export class MapPopover {
   directionsRenderer: any;
+  displayOverlay: Boolean;
+  map: any;
+  mapPolygons: any[];
+  toggleInitialized: Boolean = false;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.directionsRenderer = navParams.get("directionsRenderer");
+    this.map = navParams.get("map");
+    this.mapPolygons = navParams.get("mapPolygons");
+
+    this.displayOverlay = this.mapPolygons[0].getMap() != null;
+
+    setTimeout(() => this.toggleInitialized = true, 500);
   }
 
   clearDirections() {
     this.directionsRenderer.setMap(null);
+  }
+
+  onOverlayToggleChange() {
+    if (this.toggleInitialized && this.displayOverlay) {
+      this.mapPolygons.forEach((polygon) => polygon.setMap(this.map));
+    } else if (this.toggleInitialized) {
+      this.mapPolygons.forEach((polygon) => polygon.setMap(null));
+    }
   }
 }
